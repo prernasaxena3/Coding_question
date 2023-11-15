73ae4ed208603abf854414797437512aba28eff2
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2023-11-15 23:53:44 +0530

    Add files via upload

---
+++ Day1.cpp
@@ -0,0 +1,199 @@
+/*Little Bobby loves chocolate. He frequently goes to his favorite 5 & 10 store, Penny Auntie, to buy them. They are having a promotion at Penny Auntie. If Bobby saves enough wrappers, he can turn them in for a free chocolate.
+
+Example
+
+n = 15
+c = 3
+m = 2
+
+He has 15 to spend, bars cost 3, and he can turn in 2 wrappers to receive another bar. Initially, he buys 5 bars and has 5 wrappers after eating them. He turns in 4 of them, leaving him with 1, for 2 more bars. After eating those two, he has 3 wrappers, turns in 2 leaving him with 1 wrapper and his new bar. Once he eats that one, he has 2 wrappers and turns them in for another bar. After eating that one, he only has  wrapper, and his feast ends. Overall, he has eaten 5 + 2 + 1 + 1 = 9 bars.
+
+Function Description
+
+Complete the chocolateFeast function in the editor below.
+
+chocolateFeast has the following parameter(s):
+
+int n: Bobby’s initial amount of money
+int c: the cost of a chocolate bar
+int m: the number of wrappers he can turn in for a free bar
+Returns
+
+int: the number of chocolates Bobby can eat after taking full advantage of the promotion*/
+
+// SOLUTION 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int chocolateFeast(int n, int c, int m) {
+    int count = 0;
+    int wrapper  = 0;
+    count = n/c;
+    wrapper = count;
+    while(wrapper >= m){
+        wrapper =  wrapper-m;
+        count++;
+        wrapper++;
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int c = stoi(first_multiple_input[1]);
+
+        int m = stoi(first_multiple_input[2]);
+
+        int result = chocolateFeast(n, c, m);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/* question -2 
+A modified Kaprekar number is a positive whole number with a special property. If you square it, then split the number into two integers and sum those integers, you have the same value you started with.
+
+Consider a positive whole number n with d digits. We square n to arrive at a number that is either 2 x d digits long or (2 x d) – 1 digits long. Split the string representation of the square into two parts, l and r. The right hand part, r must be d digits long. The left is the remaining substring. Convert those two substrings back to integers, add them and see if you get n.*/
+//SOLUTION
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'kaprekarNumbers' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER p
+ *  2. INTEGER q
+ */
+
+void kaprekarNumbers(int p, int q) {
+    bool flag = false;
+    for( long int i = p;i<=q;i++){
+       long int n = i*i;
+       long int count = 0;
+       long int copy = i;
+        
+        while(copy){
+            count++;
+            copy = copy/10;
+        }
+        long int div = pow(10, count);
+       long int sum = (n/div)+(n%div);
+        if( sum == i){
+            flag = true;
+            cout<< i<<" ";
+        }
+    }
+    if( flag == false){
+        cout<<"INVALID RANGE";
+    }
+}
+
+int main()
+{
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    kaprekarNumbers(p, q);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
