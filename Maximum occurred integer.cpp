7ef74cf68bfb52d947758653a861122cc21a0137
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-06-07 22:42:19 +0530

    Maximum occurred integer.cpp

---
+++ Maximum occurred integer.cpp
@@ -0,0 +1,88 @@
+/*Given n integer ranges, the task is to return the maximum occurring integer in the given ranges. If more than one such integer exists, return the smallest one.
+The ranges are in two arrays l[] and r[].  l[i] consists of the starting point of the range and r[i] consists of the corresponding endpoint of the range & a maxx which is the maximum value of r[].
+
+For example, consider the following ranges.
+l[] = {2, 1, 3}, r[] = {5, 3, 9)
+Ranges represented by the above arrays are.
+[2, 5] = {2, 3, 4, 5}
+[1, 3] = {1, 2, 3}
+[3, 9] = {3, 4, 5, 6, 7, 8, 9}
+The maximum occurred integer in these ranges is 3.*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // l and r are input array
+    // maxx : maximum in r[]
+    // n: size of array
+    // arr[] : declared globally with size equal to maximum in l[] and r[]
+    // Function to find the maximum occurred integer in all ranges.
+    int maxOccured(int n, int l[], int r[], int maxx) {
+
+        int a[maxx + 2] = {};
+        for (int i = 0; i < n; ++i) {
+            a[l[i]]++;
+            if (r[i] + 1 <= maxx) {
+                a[r[i] + 1]--;
+            }
+        }
+        int maxCount = a[0], result = 0;
+        for (int i = 1; i <= maxx; ++i) {
+            a[i] += a[i - 1]; 
+            if (a[i] > maxCount) {
+                maxCount = a[i];
+                result = i;
+            }
+        }
+        return result;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    // taking testcases
+    cin >> t;
+
+    while (t--) {
+        int n;
+
+        // taking size of array
+        cin >> n;
+        int l[n];
+        int r[n];
+
+        // adding elements to array L
+        for (int i = 0; i < n; i++) {
+            cin >> l[i];
+        }
+
+        int maxx = 0;
+
+        // adding elements to array R
+        for (int i = 0; i < n; i++) {
+
+            cin >> r[i];
+            if (r[i] > maxx) {
+                maxx = r[i];
+            }
+        }
+        Solution ob;
+
+        // calling maxOccured() function
+        cout << ob.maxOccured(n, l, r, maxx) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
