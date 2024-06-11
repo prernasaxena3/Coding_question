92329ded954d21019df3878118b4b5a095fa87ad
Prerna Saxena <136017711+prernasaxena3@users.noreply.github.com>
2024-06-11 19:46:50 +0530

    Maximum Tip Calculator.cpp

---
+++ Maximum Tip Calculator.cpp
@@ -0,0 +1,95 @@
+//Maximum Tip Calculator
+/*In a restaurant, two waiters, A and B, receive n orders per day, earning tips as per arrays arr[i] and brr[i] respectively. If A takes the ith order, the tip is arr[i] rupees; if B takes it, the tip is brr[i] rupees.
+
+To maximize total tips, they must distribute the orders such that:
+
+A can handle at most x orders
+B can handle at most y orders
+Given that x + y ≥ n, all orders can be managed by either A or B. Return the maximum possible total tip after processing all the orders.
+
+Examples
+
+Input: n = 5, x = 3, y = 3, arr = {1, 2, 3, 4, 5}, brr = {5, 4, 3, 2, 1}
+Output: 21
+Explanation: Person A will serve the 3rd, 4th and 5th order while person B will serve the rest so the total tip from A = 3+4+5 & B = 5 + 4 i.e. 21. */
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &a, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &a[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &a) {
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long maxTip(int n, int x, int y, vector<int> &arr, vector<int> &brr) {
+        vector<pair<int, int>> diff(n);
+        for (int i = 0; i < n; ++i)
+            diff[i] = {abs(arr[i] - brr[i]), i};
+
+        sort(diff.rbegin(), diff.rend());
+
+        long long ans = 0;
+        for (const auto &p : diff) {
+            int i = p.second;
+            if ((arr[i] > brr[i] && x > 0) || y == 0) {
+                ans += arr[i];
+                --x;
+            } else {
+                ans += brr[i];
+                --y;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        int y;
+        scanf("%d", &y);
+
+        vector<int> arr(n);
+        Array::input(arr, n);
+
+        vector<int> brr(n);
+        Array::input(brr, n);
+
+        Solution obj;
+        long long res = obj.maxTip(n, x, y, arr, brr);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
